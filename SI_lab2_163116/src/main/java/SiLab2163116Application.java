f6daea88abe9b5266d884573a9062d49f8c51d3e
AlenStojmirov <stojmirovalen@gmail.com>
2020-08-25 22:46:46 +0200

    Initial commit

---
+++ SI_lab2_163116/src/main/java/SiLab2163116Application.java
@@ -0,0 +1,13 @@
+package com.example.SI_lab2_163116;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiLab2163116Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiLab2163116Application.class, args);
+	}
+
+}
---
+++ SI_lab2_163116/src/test/java/com/example/SI_lab2_163116/SiLab2163116ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.SI_lab2_163116;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SiLab2163116ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
